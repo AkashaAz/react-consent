a0d44ff3b1050945faeaad953c99ce654bc6f524
Thanapon <thanapon.makmesup@gmail.com>
2023-09-14 17:38:00 +0700

    asdasd

--- josh/src/component/layout/HeaderCartButton.tsx
+++ josh/src/component/layout/HeaderCartButton.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 
-import CartIcon from '../Cart/CartIcon';
+import CartIcon from '../cart/cartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
